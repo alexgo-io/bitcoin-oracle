4cf10409dcf397a631e8e33f36cccd17d9c1d012
Zitao Xiong <caoer115@gmail.com>
2023-09-01 23:44:34 +0800

    merge wip

--- packages/libs/types/src/index.ts
+++ packages/libs/types/src/index.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export * from './lib/api-errors';
-export * from './lib/db.types';
+export * from './lib/db-model';
 
 export const ServiceTypeSchema = z.enum(['RELAYER', 'VALIDATOR']);
 export type ServiceType = z.infer<typeof ServiceTypeSchema>;
