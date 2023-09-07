fec7f188e8145da245beabdebb2ea3080ee88c4f
Info Island <frigate.eddy-0c@icloud.com>
2023-09-07 23:56:14 +0800

    feat: mainnet testing

---
+++ packages/contracts/brc20-indexer/src/constants.ts
@@ -0,0 +1,2 @@
+export const kIndexerContractName = 'indexer-dev-preview-3' as const;
+export const kIndexerRegistryName = 'indexer-registry-dev-preview-1' as const;
--- packages/contracts/brc20-indexer/src/index.ts
+++ packages/contracts/brc20-indexer/src/index.ts
@@ -1,6 +1,6 @@
+export * from './constants';
+export * from './generated/contracts_indexer';
+export * from './operations/StacksCaller';
 export * from './operations/operationFactory';
 export * from './operations/processOperations';
-export * from './generated/contracts_indexer'
-export * from './sign/sign'
-
-export * from './operations/StacksCaller'
+export * from './sign/sign';
