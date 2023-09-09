08ea714dd0b648bf602d715faa75deba763dbaf5
Info Island <frigate.eddy-0c@icloud.com>
2023-09-09 19:51:19 +0800

    feat: queue bis

--- packages/libs/validator-bis/src/queue.ts
+++ packages/libs/validator-bis/src/queue.ts
@@ -2,10 +2,9 @@ import { getGlobalPQueue } from '@alex-b20/commons';
 
 export const getBISQueue = () => {
   return getGlobalPQueue('bis-queue', {
-    concurrency: 5,
     // per 1 minute of 50 requests
     interval: 5e3,
-    intervalCap: 3,
+    intervalCap: 4,
   });
 };
 export const getElectrumQueue = () => {
