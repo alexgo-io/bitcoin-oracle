b5dc3c3a7adf5ec0102bedf009e7a0ecb2854786
Info Island <frigate.eddy-0c@icloud.com>
2023-09-15 01:35:01 +0800

    feat: add hiro validator

--- packages/libs/validator-hiro/src/queue.ts
+++ packages/libs/validator-hiro/src/queue.ts
@@ -1,11 +1,7 @@
 import { getGlobalPQueue } from '@alex-b20/commons';
 
 export const getHiroQueue = () => {
-  return getGlobalPQueue('bis-queue', {
-    // per 1 minute of 50 requests
-    interval: 5e3,
-    intervalCap: 4,
-  });
+  return getGlobalPQueue('bis-queue', {});
 };
 export const getElectrumQueue = () => {
   return getGlobalPQueue('electrum-queue', { concurrency: 20 });
