96376bf2b9f0585c44e4cf4a8c4126cd5e1e4bbf
Zitao Xiong <caoer115@gmail.com>
2023-09-20 01:46:17 +0800

    fix: auth component

--- packages/libs/validator-bis/src/module/validator-bis.service.ts
+++ packages/libs/validator-bis/src/module/validator-bis.service.ts
@@ -1,7 +1,9 @@
 import { ValidatorProcessInterface } from '@alex-b20/validator';
+import { Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { processBlock$ } from '../validator/validator';
 
+@Injectable()
 export class DefaultValidatorBisService implements ValidatorProcessInterface {
   processBlock$(block: number): Observable<unknown> {
     return processBlock$(block);
