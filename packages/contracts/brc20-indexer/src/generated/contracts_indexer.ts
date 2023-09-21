ce8cccbc6d28dde628b7d37b9c2c5e1c7b7f6f9f
Zitao Xiong <caoer115@gmail.com>
2023-09-21 13:18:26 +0800

    chore: use default name

--- packages/contracts/brc20-indexer/src/generated/contracts_indexer.ts
+++ packages/contracts/brc20-indexer/src/generated/contracts_indexer.ts
@@ -1,12 +1,12 @@
 import { defineContract } from "../codegenImport";
 import { clarityBitcoin } from "./contract_clarity-bitcoin"
-import { indexerDevPreview4 } from "./contract_indexer-dev-preview-4"
-import { indexerRegistryDevPreview1 } from "./contract_indexer-registry-dev-preview-1"
+import { indexer } from "./contract_indexer"
+import { indexerRegistry } from "./contract_indexer-registry"
 
 export const indexerContracts = defineContract({
 ...clarityBitcoin,
-...indexerDevPreview4,
-...indexerRegistryDevPreview1
+...indexer,
+...indexerRegistry
 });
 
   
