8fc5cde4e2d4cfe5e710f6611a773e71de3a516c
Info Island <frigate.eddy-0c@icloud.com>
2023-09-21 20:26:41 +0800

    feat: add hiro module validator

--- packages/apps/validator/src/validator-module/validator.module.ts
+++ packages/apps/validator/src/validator-module/validator.module.ts
@@ -1,16 +1,18 @@
+import { PinoLoggerModule } from '@alex-b20/commons';
 import { Enums, IndexerType } from '@alex-b20/types';
 import { ValidatorBisModule } from '@alex-b20/validator-bis';
+import { ValidatorHiroModule } from '@alex-b20/validator-hiro';
 import { DynamicModule, Module } from '@nestjs/common';
 import ValidatorServiceProvider from './validator.service';
-import { PinoLoggerModule } from "@alex-b20/commons";
 
 @Module({})
 export class ValidatorModule {
   static withValidator(type: IndexerType): DynamicModule {
     const moduleMapping = {
-      [Enums.IndexerType.enum.bis]: ValidatorBisModule,
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       [Enums.IndexerType.enum.okx]: null as any,
+      [Enums.IndexerType.enum.bis]: ValidatorBisModule,
+      [Enums.IndexerType.enum.hiro]: ValidatorHiroModule,
     };
 
     const validatorModule = moduleMapping[type];
--- packages/libs/validator-hiro/src/index.ts
+++ packages/libs/validator-hiro/src/index.ts
@@ -1,2 +1,3 @@
 export * from './api';
-export * from './validator'
+export * from './module/validator-hiro.module';
+export * from './validator';
