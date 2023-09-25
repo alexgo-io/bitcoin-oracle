b7b77db65854ac465a5d92c6ee059097b6ced5b9
Zitao Xiong <caoer115@gmail.com>
2023-09-25 17:33:23 +0800

    rename

--- packages/apps/api-server/src/env.ts
+++ packages/apps/api-server/src/env.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 export const env = createEnv({
   server: {
     ENABLE_DEBUG_INFO: z.coerce.boolean().default(false),
-    ALEX_B20_API_CREDENTIALS: z.string().default('{}'),
+    BRC20_ORACLE_API_CREDENTIALS: z.string().default('{}'),
     API_PORT: z.coerce.number().default(8716),
     HEARTBEAT_URL: z.string().optional(),
     DISABLE_AUTH: z.coerce.boolean().default(false),
