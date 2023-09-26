b4bd3395877fbcac714da5bd768e5d9406ddf5b0
Info Island <frigate.eddy-0c@icloud.com>
2023-09-26 19:53:07 +0800

    feat: merge contract repo (#14)
    * chore: remove git module ref
    
    * use workspace
    
    * update code
    
    * feat: ci
    
    * chore: update lock file
    
    * remove submodule
    
    * remove indexer scripts
    
    * cache buster
    
    * cache buster

--- packages/apps/cli/src/commands/stacks/decode-index-tx-many.ts
+++ packages/apps/cli/src/commands/stacks/decode-index-tx-many.ts
@@ -1,41 +1,44 @@
 #!/usr/bin/env ts-node
 
 import { indexerContracts } from '@brc20-oracle/brc20-indexer';
-import { Args, Command, Flags } from '@oclif/core';
+import { stringifyJSON } from '@brc20-oracle/commons';
+import { Command, Flags } from '@oclif/core';
 import { hexToCV } from '@stacks/transactions';
-import { stringifyJSON } from "@brc20-oracle/commons";
-import * as fs from 'fs'
-
+import fs from 'fs';
+import { cwd } from 'node:process';
+import path from 'path';
 export default class DecodeIndexTxMany extends Command {
   static description = 'Decode the input to indexer:index-tx-many';
 
   static flags = {
     version: Flags.version(),
-    help: Flags.help(),    
+    help: Flags.help(),
     input: Flags.string({
       char: 'i',
       required: true,
     }),
     output: Flags.string({
       char: 'o',
-      required: true
-    })
+      required: true,
+    }),
   };
 
   // static args = {
   //   hex: Args.string({ description: 'the input to indexer:index-tx-many', required: true }),
   // };
-  
+
   async run(): Promise<void> {
-    const {flags} = await this.parse(DecodeIndexTxMany);
+    const { flags } = await this.parse(DecodeIndexTxMany);
     const hex = fs.readFileSync(flags.input).toString();
 
     const cv = hexToCV(hex);
     const val =
-      indexerContracts['indexer'][
-        'index-tx-many'
-      ].input[0].type.decode(cv);
+      indexerContracts['indexer']['index-tx-many'].input[0].type.decode(cv);
 
-    fs.writeFileSync(flags.output, stringifyJSON(val));
+    fs.writeFileSync(path.resolve(cwd(), flags.output), stringifyJSON(val));
+
+    this.log(
+      `Decoded input to indexer:index-tx-many and saved to ${flags.output}`,
+    );
   }
 }
