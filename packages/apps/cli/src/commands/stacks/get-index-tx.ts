53afe471970ee3179dbb396eda80cd4ad3c2a9ce
Info Island <frigate.eddy-0c@icloud.com>
2023-09-28 15:50:44 +0800

    add command to query indextx

---
+++ packages/apps/cli/src/commands/stacks/get-index-tx.ts
@@ -0,0 +1,38 @@
+import {
+  StacksCaller,
+  kIndexerRegistryName,
+} from '@bitcoin-oracle/brc20-indexer';
+import { Command, Flags } from '@oclif/core';
+import { env } from '../../env';
+import { stringifyJSON } from "@bitcoin-oracle/commons";
+
+export default class World extends Command {
+  static description = 'Say hello world';
+  private readonly stacks = new StacksCaller(
+    env().STACKS_RELAYER_ACCOUNT_SECRET!,
+    env().STACKS_DEPLOYER_ACCOUNT_ADDRESS!,
+  );
+  static flags = {
+    tx_hash: Flags.string({ required: true }),
+    satpoint: Flags.string({ required: true }),
+    output: Flags.string({ required: true }),
+  };
+
+  async run(): Promise<void> {
+    const {
+      flags: { output, satpoint, tx_hash },
+    } = await this.parse(World);
+
+    const result = await this.stacks.readonlyCaller()(
+      kIndexerRegistryName,
+      'get-bitcoin-tx-indexed-or-fail',
+      {
+        'bitcoin-tx': Buffer.from(tx_hash, 'hex'),
+        offset: BigInt(satpoint),
+        output: BigInt(output),
+      },
+    );
+
+    this.log(`result: ${stringifyJSON(result)} `);
+  }
+}
