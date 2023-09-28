3777bdac4563ce9a71d43068b7a386c89d36f264
Zitao Xiong <caoer115@gmail.com>
2023-09-28 15:50:44 +0800

    add command to query indextx

--- packages/apps/cli/src/env.ts
+++ packages/apps/cli/src/env.ts
@@ -1,9 +1,13 @@
 import { createEnv } from '@t3-oss/env-core';
 import memoizee from 'memoizee';
+import { z } from 'zod';
 
 export const env = memoizee(() =>
   createEnv({
-    server: {},
+    server: {
+      STACKS_RELAYER_ACCOUNT_SECRET: z.string().nullish(),
+      STACKS_DEPLOYER_ACCOUNT_ADDRESS: z.string().nullish(),
+    },
     runtimeEnv: process.env,
   }),
 );
