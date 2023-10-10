6c8313d8f2fb4494c52c9992f075a56e6aa38b55
Info Island <frigate.eddy-0c@icloud.com>
2023-10-10 18:19:10 +0800

    fix: test use testnet domain hash

--- packages/libs/brc20-indexer/src/sign/sign.spec.ts
+++ packages/libs/brc20-indexer/src/sign/sign.spec.ts
@@ -24,14 +24,14 @@ describe('sign', () => {
 
     const hash = structuredDataHash(encodeValue);
     expect(hash.toString('hex')).toMatchInlineSnapshot(
-      `"f62aa6e32781feb0bcbb84c2df7d9838eb2a092bc4c66ed6d6d38d2ab72f438d"`,
+      `"6d11cd301d11961e7cfeabd61e3f4da17f42f3d627362c8878aa9cbb5c532be2"`,
     );
     const signature = await signTx(
       '114dd00b2407eb036aa12c38662ed35ff3ba6c4f743b5a8ae4c984ac0ec7afe301',
       encodeValue,
     );
     expect(signature.toString('hex')).toMatchInlineSnapshot(
-      `"7e8730beb34166ab2f3c5de3d253fd8eb13a5b6a24bea9e2c85fbac519b17ee75448463a06a1a208f751259c3a1f10dfddf35ff0844a2b6ba85b50e8916c32e500"`,
+      `"cc0659d8d366e0516a8fdadde96d1ef59a8186786558d8f817e259ca8f62837e744055fb03f4690df9603fc469f81aece4e838fbde32c157a6fef905b4acf8a201"`,
     );
   });
 });
