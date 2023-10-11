6502cb001cac38b7b7bdee2d9b480b593fd0c527
Info Island <frigate.eddy-0c@icloud.com>
2023-10-11 20:40:51 +0800

    feat: support error query

--- packages/libs/commons/src/utils/global-queue.ts
+++ packages/libs/commons/src/utils/global-queue.ts
@@ -1,7 +1,7 @@
 import memoizee from 'memoizee';
 import PQueue from 'p-queue';
 
-type QueueConfig = ConstructorParameters<typeof PQueue>[0];
+export type QueueConfig = ConstructorParameters<typeof PQueue>[0];
 export const getGlobalPQueue = memoizee(
   (queueName: string, config?: QueueConfig | undefined) => {
     return new PQueue(config);
