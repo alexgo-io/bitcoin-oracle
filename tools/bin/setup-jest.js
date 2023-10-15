a7c2925ef5b494b94c86c450874cc9e123afc0dd
Info Island <frigate.eddy-0c@icloud.com>
2023-10-16 01:31:00 +0800

    chore: enforce test run against local db

--- tools/bin/setup-jest.js
+++ tools/bin/setup-jest.js
@@ -1,6 +1,11 @@
-const path = require("path");
+const path = require('path');
 require('dotenv').config({
   debug: false,
   override: true,
   path: path.resolve(__dirname, '../../.env'),
 });
+
+const DB_URL = process.env['NODE_DATABASE_URL'];
+if (!DB_URL.includes('localhost')) {
+  throw new Error('Test can only run against localhost database');
+}
