ebd2e9596b30611615440926177b5c0a96007de4
Info Island <frigate.eddy-0c@icloud.com>
2023-10-16 02:03:13 +0800

    feat: set hiro api rates

--- packages/libs/validator-hiro/src/queue.ts
+++ packages/libs/validator-hiro/src/queue.ts
@@ -2,8 +2,8 @@ import { getGlobalPQueue } from '@meta-protocols-oracle/commons';
 import PQueue from 'p-queue';
 
 export const getHiroQueue = (): PQueue => {
-  return getGlobalPQueue('bis-queue', {});
-};
-export const getElectrumQueue = (): PQueue => {
-  return getGlobalPQueue('electrum-queue', { concurrency: 20 });
+  return getGlobalPQueue('bis-queue', {
+    interval: 5e3,
+    intervalCap: 8,
+  });
 };
