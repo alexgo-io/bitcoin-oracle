ac104f6f412719624c65dce64a56f2e079889172
fiftyeightandeight <alexd@alexgo.io>
2023-10-27 14:22:52 +0800

    fix: hash-tx throws unwrap-panic

--- packages/contracts/brc20-indexer-v1/scripts/sign-tx-hash.ts
+++ packages/contracts/brc20-indexer-v1/scripts/sign-tx-hash.ts
@@ -5,23 +5,23 @@
 // These are helper scripts to make development a little bit easier.
 // DO NOT USE REAL SEED PHRASES OR PRIVATE KEYS.
 
-import { StacksMainnet, StacksMocknet } from "@stacks/network";
+import { StacksMocknet } from '@stacks/network';
 import {
   ClarityValue,
+  StacksPrivateKey,
   createStacksPrivateKey,
   serializeCV,
   signWithKey,
-  StacksPrivateKey,
   stringAsciiCV,
   tupleCV,
   uintCV,
-} from "@stacks/transactions";
-import { createHash } from "crypto";
+} from '@stacks/transactions';
+import { createHash } from 'crypto';
 
 const structuredDataPrefix = Buffer.from([0x53, 0x49, 0x50, 0x30, 0x31, 0x38]);
 
 function sha256(data: Buffer): Buffer {
-  return createHash("sha256").update(data).digest();
+  return createHash('sha256').update(data).digest();
 }
 
 function structuredDataHash(structuredData: ClarityValue): Buffer {
@@ -30,10 +30,10 @@ function structuredDataHash(structuredData: ClarityValue): Buffer {
 
 const domainHash = structuredDataHash(
   tupleCV({
-    name: stringAsciiCV("ALEX BRC20 Indexer"),
-    version: stringAsciiCV("0.0.1"),
-    "chain-id": uintCV(new StacksMainnet().chainId),
-    // 'chain-id': uintCV(new StacksMocknet().chainId),
+    name: stringAsciiCV('ALEX BRC20 Indexer'),
+    version: stringAsciiCV('0.0.1'),
+    // "chain-id": uintCV(new StacksMainnet().chainId),
+    'chain-id': uintCV(new StacksMocknet().chainId),
   }),
 );
 
@@ -45,8 +45,8 @@ function signStructuredData(
   const input = sha256(
     Buffer.concat([structuredDataPrefix, domainHash, messageHash]),
   );
-  const data = signWithKey(privateKey, input.toString("hex")).data;
-  return Buffer.from(data.slice(2) + data.slice(0, 2), "hex");
+  const data = signWithKey(privateKey, input.toString('hex')).data;
+  return Buffer.from(data.slice(2) + data.slice(0, 2), 'hex');
 }
 
 if (process.argv.length !== 4) {
@@ -56,8 +56,8 @@ if (process.argv.length !== 4) {
 
 function toBuffer(input: string) {
   return Buffer.from(
-    input.length >= 2 && input[1] === "x" ? input.slice(2) : input,
-    "hex",
+    input.length >= 2 && input[1] === 'x' ? input.slice(2) : input,
+    'hex',
   );
 }
 
@@ -67,16 +67,16 @@ const hash = process.argv[3]!;
 const stacksPrivateKey = createStacksPrivateKey(toBuffer(private_key));
 
 function signTxHash(private_key: StacksPrivateKey, hash: Buffer) {
-  const message = createHash("sha256")
+  const message = createHash('sha256')
     .update(Buffer.concat([structuredDataPrefix, domainHash, hash]))
     .digest();
-  const data = signWithKey(private_key, message.toString("hex")).data;
-  return Buffer.from(data.slice(2) + data.slice(0, 2), "hex");
+  const data = signWithKey(private_key, message.toString('hex')).data;
+  return Buffer.from(data.slice(2) + data.slice(0, 2), 'hex');
 }
 
-console.log(`Structured Data Prefix:  ${structuredDataPrefix.toString("hex")}`);
-console.log(`Domain Hash:             ${domainHash.toString("hex")}`);
+console.log(`Structured Data Prefix:  ${structuredDataPrefix.toString('hex')}`);
+console.log(`Domain Hash:             ${domainHash.toString('hex')}`);
 
 const signature = signTxHash(stacksPrivateKey, toBuffer(hash));
 
-console.log(`Signature:               0x${signature.toString("hex")}`);
+console.log(`Signature:               0x${signature.toString('hex')}`);
