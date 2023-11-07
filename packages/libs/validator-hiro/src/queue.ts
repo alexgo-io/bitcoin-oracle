6dc6c37cc24b4b0ea4aefe2eb5a98689b1fd703f
Info Island <frigate.eddy-0c@icloud.com>
2023-11-08 00:35:50 +0800

    feat: add queue limit to hiro

--- packages/libs/validator-hiro/src/queue.ts
+++ packages/libs/validator-hiro/src/queue.ts
@@ -4,5 +4,7 @@ import PQueue from 'p-queue';
 export const getHiroQueue = (): PQueue => {
   return getGlobalPQueue('bis-queue', {
     concurrency: 20,
+    interval: 10e3,
+    intervalCap: 4,
   });
 };
