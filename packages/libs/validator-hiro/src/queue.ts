6b4bfda86af0c38e33515b86f8e4376032abf6bd
Info Island <frigate.eddy-0c@icloud.com>
2023-11-08 02:38:24 +0800

    feat: add configurable rate limit

--- packages/libs/validator-hiro/src/queue.ts
+++ packages/libs/validator-hiro/src/queue.ts
@@ -1,10 +1,11 @@
 import { getGlobalPQueue } from '@meta-protocols-oracle/commons';
 import PQueue from 'p-queue';
+import { env } from './env';
 
 export const getHiroQueue = (): PQueue => {
   return getGlobalPQueue('bis-queue', {
     concurrency: 20,
-    interval: 10e3,
-    intervalCap: 4,
+    interval: 5e3,
+    intervalCap: Math.floor(env().RATE_LIMIT_PER_MINUTE / 5),
   });
 };
