9eac1f8129fd245cff0ba1eb971aed62b52d4105
Info Island <frigate.eddy-0c@icloud.com>
2024-01-02 11:22:36 +0800

    chore: better naming

--- packages/libs/brc20-indexer/src/generated/index.ts
+++ packages/libs/brc20-indexer/src/generated/index.ts
@@ -2,7 +2,6 @@ import * as oracleRegistry from './contract_oracle-registry-v1-01';
 import * as oracle from './contract_oracle-v1-03';
 
 export const OracleContracts = {
-  'oracle-v1-03': oracle.oracleV103['oracle-v1-03'],
-  'oracle-registry-v1-01':
-    oracleRegistry.oracleRegistryV101['oracle-registry-v1-01'],
+  ...oracle.oracleV103,
+  ...oracleRegistry.oracleRegistryV101,
 };
